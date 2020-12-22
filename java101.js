6b0a9a381fe35555cd855245ba528b1042647e9d
sirima2547 <69945455+sirima2547@users.noreply.github.com>
2020-12-22 11:12:29 +0700

    Add files via upload

---
+++ java101.js
@@ -0,0 +1,35 @@
+function Function3(){
+    let x = document.getElementById("myBtn3").value;
+    //document.getElementById("images").src="red.PNG";
+    document.getElementById("images").src = x;
+}
+function Function4(){
+    let x = document.getElementById("myBtn4").value;
+    //document.getElementById("images"). src=""green.PNG"
+    document.getElementById("images").src = x;
+}
+function Function5(){
+    let x = document.getElementById("myBtn5").value;
+    //document.getElementById("images"). src="blue.PNG"
+    document.getElementById("images").src = x;
+}
+
+function Function6(){
+    document.getElementById("text").style.color="green";
+}
+
+function Function7(){
+    document.getElementById("text").style.color="blue";
+}
+
+function Function8(){
+    document.getElementById("text").style.color="brown";
+}
+
+function Function9(){
+    document.getElementById("hideDisplay").style.display="none";
+}
+
+function Function10(){
+    document.getElementById("hideDisplay").style.display="block";
+}
